3752d59fae12cc897d0b89d7fdb275dadab1b462
derekearl <ear17004@byui.edu>
2023-07-19 09:59:34 -0600

    Final For Sure

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import bootstrap from "bootstrap";
-// import { Carousel } from "bootstrap";
+// Import { Carousel } from "bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
